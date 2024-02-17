5d0b53ec648280e0cde294e6adc6ab61c186f3a6
HullHound <Hullhound@hotmail.com>
2024-02-17 11:51:20 +0000

    Added Goblin base and Resolve Invert Anim Issues

---
+++ data/scenes/goblin.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+signal died;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func die():
+	died.emit()
+
+func _on_damage_target_2d_damaged(damage_amount: float) -> void:
+	$AnimationPlayer.play("damaged")
