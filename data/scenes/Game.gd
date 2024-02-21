f09bf5d607f600994a201e05f531f89e3f6d88dc
HullHound <Hullhound@hotmail.com>
2024-02-21 08:21:35 +0000

    End Screen shows in full screen

--- data/scenes/Game.gd
+++ data/scenes/Game.gd
@@ -8,11 +8,11 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	pass
 	
-func _on_player_dungeon_heart_died() -> void:
-	print('player died')
+func _on_player_dungeon_heart_died() -> void:	
+	$PlayerInteraction/UI/EndScreen.showScreen("You have been defeated", "Try again?")
 
 func _on_enemy_dungeon_heart_died() -> void:
-	$UI/EndScreen.showScreen("You have conquered this realm!", "Play again?")
+	$PlayerInteraction/UI/EndScreen.showScreen("You have conquered this realm!", "Play again?")
 
 func _on_end_screen_restart_requested() -> void:
 	get_tree().reload_current_scene()
