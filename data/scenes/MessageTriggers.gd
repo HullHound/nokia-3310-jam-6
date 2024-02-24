a8ff987d2c54a66f2cb6e2c7a414616f686b7852
HullHound <Hullhound@hotmail.com>
2024-02-24 20:20:20 +0000

    Added random quips

--- data/scenes/MessageTriggers.gd
+++ data/scenes/MessageTriggers.gd
@@ -1,10 +1,11 @@
 extends Node
 
+@export var random_quips: Array[String]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GoldTreasury.not_enough_gold.connect(not_enough_gold)
-
+	MessageSystem.no_message_gap.connect(no_message_gap)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,3 +13,6 @@ func _process(delta: float) -> void:
 
 func not_enough_gold():
 	MessageSystem.displayMessage("You don't have enough gold")
+
+func no_message_gap():
+	MessageSystem.displayMessage(random_quips.pick_random())
--- message_system.gd
+++ message_system.gd
@@ -1,6 +1,22 @@
 extends Node
 
+@export var no_message_gap_window = 30.0
+
+var time_since_last_message = 0.0
+var sent_no_message_signal = false
+
 signal new_message(message: String)
+signal no_message_gap
 
 func displayMessage(message):
 	new_message.emit(message)
+	time_since_last_message = 0.0
+	sent_no_message_signal = false
+
+func _physics_process(delta: float) -> void:
+	time_since_last_message += delta
+	
+	if time_since_last_message >= no_message_gap_window:
+		sent_no_message_signal = true
+		no_message_gap.emit()	
+
